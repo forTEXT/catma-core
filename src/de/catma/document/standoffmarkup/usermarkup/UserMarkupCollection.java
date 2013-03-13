2a6b23cf6c87ec7d8f7b6bc734731152412a7639
Marco Petris <marco.petris@web.de>
2013-03-13 17:09:47 +0100

    id based identity to enable sharing

--- src/de/catma/document/standoffmarkup/usermarkup/UserMarkupCollection.java
+++ src/de/catma/document/standoffmarkup/usermarkup/UserMarkupCollection.java
@@ -127,6 +127,7 @@ public class UserMarkupCollection {
 		this.tagLibrary = tagLibrary;
 	}
 
+	@Deprecated
 	public void setId(String id) {
 		this.id = id;
 	}
@@ -212,4 +213,36 @@ public class UserMarkupCollection {
 		}
 		return null;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		UserMarkupCollection other = (UserMarkupCollection) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
 }
