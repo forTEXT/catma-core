dafcfb013931897a2817d07b19c7c967f3bfbcd2
Marco Petris <marco.petris@web.de>
2013-10-03 18:50:16 +0200

    collection utilities

--- src/de/catma/util/Collections3.java
+++ src/de/catma/util/Collections3.java
@@ -1,6 +1,7 @@
 package de.catma.util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -15,5 +16,16 @@ public class Collections3 {
 		}
 		
 		return sDiff;
+	}
+	
+	public static <T> Collection<T> getUnion(Collection<T> col1, Collection<T> col2) {
+		List<T> union = new ArrayList<T>();
+		union.addAll(col1);
+		union.addAll(col2);
+		return union;
+	}
+	
+	public static <T> Collection<T> getUnion(T[] col1, T[] col2) {
+		return getUnion(Arrays.asList(col1), Arrays.asList(col2));
 	}
 }
