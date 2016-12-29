837437c4de0d313d602112d11545d2f569d72311
Marco Petris <marco.petris@web.de>
2016-12-30 00:02:07 +0100

    advanced retrieval methods for tagsetdefs

--- src/de/catma/document/repository/Repository.java
+++ src/de/catma/document/repository/Repository.java
@@ -44,6 +44,7 @@ import de.catma.tag.TagLibrary;
 import de.catma.tag.TagLibraryReference;
 import de.catma.tag.TagManager;
 import de.catma.tag.TagsetDefinition;
+import de.catma.tag.Version;
 import de.catma.user.User;
 import de.catma.util.Pair;
 
@@ -466,5 +467,6 @@ public interface Repository {
 	public int getNewUserMarkupCollectionRefs(Corpus corpus);
 	
 	public void spawnContentFrom(String userIdentifier, boolean copyCorpora, boolean copyTagLibs) throws IOException;
+	public TagLibrary getTagLibraryFor(String uuid, Version version) throws IOException;
 	
 }
