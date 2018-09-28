525a0b2fdac1e2513ef11231e2a7397ae9100729
Marco Petris <marco.petris@web.de>
2018-09-28 22:40:44 +0200

    TagQuery Cypher implementation

--- src/main/java/de/catma/tag/TagInstance.java
+++ src/main/java/de/catma/tag/TagInstance.java
@@ -176,4 +176,31 @@ public class TagInstance {
 		}
 		return getUserDefinedProperty(uuid);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TagInstance other = (TagInstance) obj;
+		if (uuid == null) {
+			if (other.uuid != null)
+				return false;
+		} else if (!uuid.equals(other.uuid))
+			return false;
+		return true;
+	}
+	
+	
 }
