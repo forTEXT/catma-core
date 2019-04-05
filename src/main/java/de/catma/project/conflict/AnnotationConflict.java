86650a06fb19af3941c20199247faf8fa6b484f8
Marco Petris <marco.petris@web.de>
2019-04-05 10:23:08 +0200

    conflict resolution

--- src/main/java/de/catma/project/conflict/AnnotationConflict.java
+++ src/main/java/de/catma/project/conflict/AnnotationConflict.java
@@ -8,8 +8,37 @@ import de.catma.tag.TagInstance;
 public class AnnotationConflict {
 
 	private TagInstance devTagInstance;
-	private TagInstance masterTagInstance;
 	private List<TagReference> devTagReferences;
+
+	private TagInstance masterTagInstance;
 	private List<TagReference> masterTagReferences;
+	
+	
+	public AnnotationConflict(TagInstance devTagInstance, List<TagReference> devTagReferences,
+			TagInstance masterTagInstance, List<TagReference> masterTagReferences) {
+		super();
+		this.devTagInstance = devTagInstance;
+		this.devTagReferences = devTagReferences;
+		this.masterTagInstance = masterTagInstance;
+		this.masterTagReferences = masterTagReferences;
+	}
+	
+	
+	public TagInstance getDevTagInstance() {
+		return devTagInstance;
+	}
+	
+	public List<TagReference> getDevTagReferences() {
+		return devTagReferences;
+	}
+	
+	public TagInstance getMasterTagInstance() {
+		return masterTagInstance;
+	}
+	
+	public List<TagReference> getMasterTagReferences() {
+		return masterTagReferences;
+	}
+	
 	
 }
--- src/main/java/de/catma/project/conflict/ConflictedProject.java
+++ src/main/java/de/catma/project/conflict/ConflictedProject.java
@@ -1,5 +1,12 @@
 package de.catma.project.conflict;
 
+import java.util.Collection;
+import java.util.List;
+
+import de.catma.document.source.SourceDocument;
+import de.catma.tag.TagDefinition;
+import de.catma.tag.TagsetDefinition;
+
 public interface ConflictedProject {
 
 //	public boolean hasConflictedAnnotations();
@@ -8,4 +15,10 @@ public interface ConflictedProject {
 //	public boolean hasConflictedCollectionMetadata();
 //	public boolean hasConflictedTag();
 //	public boolean hasConflictedTagsetMetadata();
+	
+	public List<CollectionConflict> getCollectionConflicts() throws Exception;
+	public Collection<TagsetDefinition> getTagsets() throws Exception;
+	Collection<SourceDocument> getDocuments() throws Exception;
+	
+	
 }
